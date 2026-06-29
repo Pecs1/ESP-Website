b2878b6699ef879733909778671bbbe8d958637a
Pecs1 <173087731+Pecs1@users.noreply.github.com>
2026-06-29 17:50:27 +0200

    feat: navigational panel

--- website/src/lib/index.ts
+++ website/src/lib/index.ts
@@ -1,1 +1,4 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+import '$lib/styles/main.css';
+export { default as favicon } from '$lib/assets/favicon.svg';
--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import { favicon } from '$lib';
 
 	let { children } = $props();
 </script>
@@ -9,3 +9,18 @@
 </svelte:head>
 
 {@render children()}
+
+<div class="dashboard-root">
+     <div class="nav-bar">
+          <div class="title">
+               <h>Telemetry Dashboard</h>
+          </div>
+          <div class="menu">
+               <svg width="60" height="60" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <line x1="3" y1="12" x2="21" y2="12"></line>
+                    <line x1="3" y1="6" x2="21" y2="6"></line>
+                    <line x1="3" y1="18" x2="21" y2="18"></line>
+               </svg>
+          </div>
+     </div>
+</div>
