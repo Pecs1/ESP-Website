6ce269ffc6ad4245491dffac153a22d9cf267a63
Pecs1 <173087731+Pecs1@users.noreply.github.com>
2026-06-29 21:38:15 +0200

    renamed main.css to layout.css

--- website/src/lib/index.ts
+++ website/src/lib/index.ts
@@ -1,4 +1,5 @@
 // place files you want to import through the `$lib` alias in this folder.
 
-import '$lib/styles/main.css';
+import '$lib/styles/layout.css';
+
 export { default as favicon } from '$lib/assets/favicon.svg';
