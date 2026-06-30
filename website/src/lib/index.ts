c60a24b81c5f193c7a25a66f25aa4c6a1e424487
Pecs1 <173087731+Pecs1@users.noreply.github.com>
2026-06-30 12:49:46 +0200

    refactor: put :root css variables into a separate file

--- website/src/lib/index.ts
+++ website/src/lib/index.ts
@@ -1,5 +1,10 @@
-// place files you want to import through the `$lib` alias in this folder.
-
+// styles
+import '$lib/styles/global.css'
 import '$lib/styles/layout.css';
 
+
+// svelte components
+
+
+// other deps
 export { default as favicon } from '$lib/assets/favicon.svg';
